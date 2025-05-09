9ca1511945c11158d71a65048ebc1c41754d2ded
Igor Almeida <igoralmeida@Igors-MacBook-Pro.local>
2025-05-09 08:58:41 +0100

    Refactor: Melhora exibição de claimed_by e carregamento do .env

--- infantinho3/settings.py
+++ infantinho3/settings.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 from django.urls import reverse_lazy # Use reverse_lazy for settings
 
-load_dotenv()
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
